72d55dd887f7a16e79ac0e1eb1f41898f8ecbf1d
kunalnegi07 <kunal82857@gmail.com>
2026-02-20 01:43:43 +0530

    add function resize to create divs

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("k")
+const container = document.querySelector(".container")
+const resizeBtn = document.querySelector("#resize");
+
+resizeBtn.addEventListener("click",resize);
+
+function resize (){
+    let NoOfGrid = Number(prompt("How many grid's you want? (2-100)"));
+    if(NoOfGrid >=2 && NoOfGrid <=100){
+        for(i = 0; i<NoOfGrid ; i++){
+        const newDiv = document.createElement("div");
+        container.appendChild(newDiv);
+    }
+    }
+    else{
+        alert("Invalid! Give a number beween 2-100");
+    }
+}
