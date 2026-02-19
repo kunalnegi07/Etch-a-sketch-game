626142dad54cbe99489a30eb17186f22ca902cd8
kunalnegi07 <kunal82857@gmail.com>
2026-02-20 02:19:54 +0530

    make div form square grid using the resize function

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const container = document.querySelector(".container")
 const resizeBtn = document.querySelector("#resize");
+const newDiv = document.createElement("div");
 
 resizeBtn.addEventListener("click",resize);
+
+
 
 function resize (){
     let NoOfGrid = Number(prompt("How many grid's you want? (2-100)"));
     if(NoOfGrid >=2 && NoOfGrid <=100){
-        for(i = 0; i<NoOfGrid ; i++){
+
+        container.textContent = "";
+
+        const size = 500/NoOfGrid;
+
+        for(i = 0; i<NoOfGrid*NoOfGrid ; i++){
         const newDiv = document.createElement("div");
+        newDiv.classList.add("division");
+        newDiv.style.width = `${size}px`;
+        newDiv.style.height = `${size}px`;
         container.appendChild(newDiv);
-    }
+        }
+        
     }
     else{
         alert("Invalid! Give a number beween 2-100");
