19cec08e5ff0b5f58a243e9fe8d0fa435d081b23
kunalnegi07 <kunal82857@gmail.com>
2026-02-23 20:26:24 +0530

    add black and rainbow color buttons

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector(".container");
 const resizeBtn = document.querySelector("#resize");
 const cleargame = document.querySelector("#clear");
+const rainbowbtn = document.querySelector("#rainbow");
+const blackbtn = document.querySelector("#black");
 
 container.style.userSelect = "none";
 let mouseDown = false;
+let currentMode = "black";
 
 document.body.addEventListener("mousedown" , () => {
     mouseDown = true;
@@ -15,10 +18,11 @@ document.body.addEventListener("mouseup" , () => {
 
 resizeBtn.addEventListener("click",resize);
 
+rainbowbtn.addEventListener("click",() => currentMode = "rainbow");
 
-function resize (){
-    let NoOfGrid = Number(prompt("How many grid's you want? (2-100)","30"));
-    if(NoOfGrid >=2 && NoOfGrid <=100){
+blackbtn.addEventListener("click", () => currentMode = "black");
+
+function createGrid(NoOfGrid){
 
         container.textContent = "";
 
@@ -35,57 +39,42 @@ function resize (){
         container.appendChild(newDiv);
 
         }
-        
+}
+
+function resize (){
+    let NoOfGrid = Number(prompt("How many grid's you want? (2-100)","30"));
+    if(NoOfGrid >=2 && NoOfGrid <=100){
+        createGrid(NoOfGrid);
     }
+
     else{
         alert("Invalid! Give a number beween 2-100");
     }
 }
 
+
 function changeColor(e){
-    if(!mouseDown) return;
+
+    if (e.type === "mouseover" && !mouseDown) return;
+
+    if(currentMode==="rainbow"){
+        e.target.style.backgroundColor = generatingRandomRgbColor();
+    }else{
         e.target.style.backgroundColor = "black";
-    
+    }
 }
 
 window.onload = function(){
-    NoOfGrid = 30;
-     if(NoOfGrid >=2 && NoOfGrid <=100){
-
-        container.textContent = "";
-
-        const size = 500/NoOfGrid;
-
-        for(i = 0; i<NoOfGrid*NoOfGrid ; i++){
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("division");
-        newDiv.style.width = `${size}px`;
-        newDiv.style.height = `${size}px`;
-        newDiv.addEventListener("mouseover",changeColor);
-        newDiv.style.backgroundColor = "white";
-        newDiv.style.border = "none";
-        container.appendChild(newDiv);
-        }
-    }
+    createGrid(30);
 }
 
-cleargame.addEventListener("click",window.onload = function(){
-    NoOfGrid = 30;
-     if(NoOfGrid >=2 && NoOfGrid <=100){
+function generatingRandomRgbColor(){
+    const r = Math.floor(Math.random() * 256); 
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
 
-        container.textContent = "";
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
-        const size = 500/NoOfGrid;
+cleargame.addEventListener("click", () => window.onload = createGrid(30));
 
-        for(i = 0; i<NoOfGrid*NoOfGrid ; i++){
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("division");
-        newDiv.style.width = `${size}px`;
-        newDiv.style.height = `${size}px`;
-        newDiv.addEventListener("mouseover",changeColor);
-        newDiv.style.backgroundColor = "white";
-        newDiv.style.border = "none";
-        container.appendChild(newDiv);
-        }
-    }
-});
